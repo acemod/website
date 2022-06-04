7b80a5fbedab56ae794aea0a2993759faecb323b
bux <github@jonathandavid.de>
2022-06-04 13:01:52 +0200

    cleanup

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,5 @@
 <template>
-  <div>
-    <Suspense>
-      <Logos mb-6 />
-      <template #fallback>
-        <div op50 italic>
-          <span animate-pulse>Loading...</span>
-        </div>
-      </template>
-    </Suspense>
-    <InputEntry />
-  </div>
+    <div>
+        <h1>Test</h1>
+    </div>
 </template>
