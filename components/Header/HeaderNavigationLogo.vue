f78ac3fcd185954dbf82de37c8ec08c083ac2be1
bux <github@jonathandavid.de>
2022-06-05 11:06:08 +0200

    fix logo url

--- components/Header/HeaderNavigationLogo.vue
+++ components/Header/HeaderNavigationLogo.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="relative w-100px h-23px">
-        <img class="logo-white mr-2 absolute top-0 left-0 transition-opacity" src="ace3-logo-white-tiny.webp" alt="ACE logo" height="23" width="100">
-        <img class="logo-black mr-2 absolute top-0 left-0 transition-opacity" src="ace3-logo-black-tiny.webp" alt="ACE logo" height="23" width="100">
+        <img class="logo-white mr-2 absolute top-0 left-0 transition-opacity" src="/ace3-logo-white-tiny.webp" alt="ACE logo" height="23" width="100">
+        <img class="logo-black mr-2 absolute top-0 left-0 transition-opacity" src="/ace3-logo-black-tiny.webp" alt="ACE logo" height="23" width="100">
     </div>
 </template>
 
